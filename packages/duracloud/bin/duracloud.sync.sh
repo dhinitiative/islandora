0bdb8cd70ac059ea8b33ff16e68ab46bf6c98f44
Steve Young <slyoung@hamilton.edu>
2015-04-01 12:59:11 -0400

    Create duracloud.sync.sh

---
+++ packages/duracloud/bin/duracloud.sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+   
+. ../etc/duracloud.config
+
+java -jar ${DURACLOUD_HOME}/lib/duracloudsync-3.2.1.jar -x \
+-t ${DURACLOUD_THREADS} \
+-c ${DURACLOUD_CONTENT} \
+-w ${DURACLOUD_WORK}/duracloud-sync-work \
+-h ${DURACLOUD_HOST} \
+-s ${DURACLOUD_SPACE} \
+-u ${DURACLOUD_USER} >> ${DURACLOUD_HOME}/logs/duracloud-sync.log 2>&1 &
