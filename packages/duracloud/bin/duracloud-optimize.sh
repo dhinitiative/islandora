a6293f0ee11602419eba7ac0c6200807c38eba97
Steve Young <slyoung@hamilton.edu>
2015-04-01 16:05:52 -0400

    Update duracloud-optimize.sh

--- packages/duracloud/bin/duracloud-optimize.sh
+++ packages/duracloud/bin/duracloud-optimize.sh
@@ -8,7 +8,7 @@ echo "Starting on:  `date`" >> $DURACLOUD_HOME/logs/duracloud-optimize.log
 java -jar ${DURACLOUD_HOME}/lib/syncoptimize-3.2.0-driver.jar \
 -h ${DURACLOUD_HOST} \
 -s ${DURACLOUD_SPACE} \
--u ${DURACLOUD_USER} >> ${DURACLOUD_HOME}/logs/duracloud-optimize.log 2>&1 &
+-u ${DURACLOUD_USER} >> ${DURACLOUD_HOME}/logs/duracloud-optimize.log 2>&1
 
 
 echo "Finished on:  `date`" >> $DURACLOUD_HOME/logs/duracloud-optimize.log
