7205aa599f74a628850aa7a09ee80992b72c22d1
Steve Young <slyoung@hamilton.edu>
2015-04-01 13:08:59 -0400

    Update get_files.sh

--- packages/duracloud/lib/get_files.sh
+++ packages/duracloud/lib/get_files.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+wget -N -P /opt/duracloud/lib http://docs.duraspace.org/duracloud/3.2.1/downloads/duracloudsync-3.2.1.jar
+wget -N -P /opt/duracloud/lib http://docs.duraspace.org/duracloud/3.2.0/downloads/retrievaltool-3.2.0-driver.jar
+wget -N -P /opt/duracloud/lib http://docs.duraspace.org/duracloud/3.2.0/downloads/syncoptimize-3.2.0-driver.jar
