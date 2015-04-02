1af42c25641bf2181c82eb771bb1b8cccd024af7
Steve Young <slyoung@hamilton.edu>
2015-04-02 09:46:40 -0400

    Update duracloud-sync.sh

--- packages/duracloud/bin/duracloud-sync.sh
+++ packages/duracloud/bin/duracloud-sync.sh
@@ -7,6 +7,7 @@ echo "Starting on:  `date`" >> $DURACLOUD_HOME/logs/duracloud-sync.log
 java -jar ${DURACLOUD_HOME}/lib/duracloudsync-3.2.1.jar -x \
 -t ${DURACLOUD_THREADS} \
 -c ${DURACLOUD_CONTENT} \
+-e ${DURACLOUD_EXCLUDE} \
 -w ${DURACLOUD_WORK}/duracloud-sync-work \
 -h ${DURACLOUD_HOST} \
 -s ${DURACLOUD_SPACE} \
