7d8b4fa933fb7fa12e2ca86d8070d88e39282363
Steve Young <slyoung@hamilton.edu>
2015-04-07 15:59:11 -0400

    Update duracloud-retrieve.sh

--- packages/duracloud/bin/duracloud-retrieve.sh
+++ packages/duracloud/bin/duracloud-retrieve.sh
@@ -3,7 +3,7 @@
 . ../etc/duracloud.config
 
 
-echo "Starting on:  `date`" >> $DURACLOUD_HOME/logs/duracloud-retrieve.log
+echo "Starting on:  `date`" | tee $DURACLOUD_HOME/logs/duracloud-retrieve.log
 
 java -jar ${DURACLOUD_HOME}/lib/retrievaltool-3.2.0-driver.jar \
 -t ${DURACLOUD_THREADS} \
@@ -11,6 +11,6 @@ java -jar ${DURACLOUD_HOME}/lib/retrievaltool-3.2.0-driver.jar \
 -w ${DURACLOUD_WORK}/duracloud-retrieve-work \
 -h ${DURACLOUD_HOST} \
 -s ${DURACLOUD_SPACE} \
--u ${DURACLOUD_USER} >> ${DURACLOUD_HOME}/logs/duracloud-retrieve.log 2>&1
+-u ${DURACLOUD_USER} 2>&1 | tee $DURACLOUD_HOME/logs/duracloud-retrieve.log
 
-echo "Finished on:  `date`" >> $DURACLOUD_HOME/logs/duracloud-retrieve.log
+echo "Finished on:  `date`" | tee $DURACLOUD_HOME/logs/duracloud-retrieve.log
