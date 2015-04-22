ce60f905cfdc48b35da19d925adb730d4cad4f31
Steve Young <slyoung@hamilton.edu>
2015-04-22 12:08:17 -0400

    Update export_collections.sh

--- bin/export_collections.sh
+++ bin/export_collections.sh
@@ -8,22 +8,23 @@
 . ../etc/database.sh
 
 EXPORT_DIR=$1
+LOG_FILE=${FEDORA_HOME}/server/logs/fedora-export.log
 
-echo "Found the following Namespaces:"
-echo ""
-${ISLANDORA_HOME}/bin/get_namespaces.sh
+echo "Found the following Namespaces:" 2>&1 | tee $LOG_FILE
+echo "" 2>&1 | tee $LOG_FILE
+${ISLANDORA_HOME}/bin/get_namespaces.sh 2>&1 | tee $LOG_FILE
 
 for namespace in `cat ${ISLANDORA_HOME}/etc/namespaces.conf`;
 do
 	collection=`echo $namespace | awk -F: '{print $1}'`
-	echo "Exporting Collection: $collection"
-	echo ""
-	mkdir $EXPORT_DIR/$collection
+	echo "Exporting Collection: $collection" 2>&1 | tee $LOG_FILE
+	echo "" 2>&1 | tee $LOG_FILE
+	mkdir $EXPORT_DIR/$collection 2>&1 | tee $LOG_FILE
 	for pid in `${ISLANDORA_HOME}/bin/find_pids.sh $collection`;
 	do
-		${FEDORA_HOME}/client/bin/fedora-export.sh ${FULL_SERVER_NAME} 8080 {$FEDORA_ADMIN_USER} ${FEDORA_ADMIN_PASS} $collection info:fedora/fedora-system:FOXML-1.1 archive $EXPORT_DIR/$collection http
+		${FEDORA_HOME}/client/bin/fedora-export.sh ${FULL_SERVER_NAME} 8080 {$FEDORA_ADMIN_USER} ${FEDORA_ADMIN_PASS} $collection info:fedora/fedora-system:FOXML-1.1 archive $EXPORT_DIR/$collection http 2>&1 | tee $LOG_FILE
 	done
-	echo ""
-	echo "Done exporting $collection"
-	echo ""
+	echo "" 2>&1 | tee $LOG_FILE
+	echo "Done exporting $collection" 2>&1 | tee $LOG_FILE
+	echo "" 2>&1 | tee $LOG_FILE
 done
