3933673eb3a478950e87069137cb90ff6e33e4c1
Steve Young <slyoung@hamilton.edu>
2015-04-28 14:32:00 -0400

    Update get_namespaces.sh

--- bin/get_namespaces.sh
+++ bin/get_namespaces.sh
@@ -6,5 +6,5 @@
 . ../etc/env.sh
 . ../etc/database.sh
 
-${FEDORA_HOME}/client/bin/fedora-find.sh ${FULL_SERVER_NAME} 8080 {$FEDORA_ADMIN_USER} ${FEDORA_ADMIN_PASS} "pid" "*:root" http | grep pid | awk '{print $2}' > ${ISLANDORA_HOME}/etc/namespaces.conf
+${FEDORA_HOME}/client/bin/fedora-find.sh ${FULL_SERVER_NAME} 8080 ${FEDORA_ADMIN_USER} ${FEDORA_ADMIN_PASS} "pid" "*:root" http | grep pid | awk '{print $2}' > ${ISLANDORA_HOME}/etc/namespaces.conf
 cat ${ISLANDORA_HOME}/etc/namespaces.conf
