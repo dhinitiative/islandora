749e07d0da1a0b0f6fbc62c39dafe9102aca95bb
Steve Young <slyoung@hamilton.edu>
2015-04-30 10:35:51 -0400

    Update export_collection.sh

--- bin/export_collection.sh
+++ bin/export_collection.sh
@@ -13,17 +13,17 @@ EXPORT_DIR=$1
 COLLECTION=$2
 LOG_FILE=${FEDORA_HOME}/server/logs/fedora-export.log
 
-echo "Starting fedora export on `date`" 2>&1 | tee $LOG_FILE
-echo "Exporting Collection: $COLLECTION" 2>&1 | tee $LOG_FILE
-	echo "" 2>&1 | tee $LOG_FILE
-	mkdir $EXPORT_DIR/$COLLECTION 2>&1 | tee $LOG_FILE
+echo "Starting fedora export on `date`" 2>&1 | tee -a $LOG_FILE
+echo "Exporting Collection: $COLLECTION" 2>&1 | tee -a $LOG_FILE
+	echo "" 2>&1 | tee -a $LOG_FILE
+	mkdir $EXPORT_DIR/$COLLECTION 2>&1 | tee -a $LOG_FILE
 	for pid in `${ISLANDORA_HOME}/bin/find_pids.sh $COLLECTION`;
 	do
-		${FEDORA_HOME}/client/bin/fedora-export.sh ${FULL_SERVER_NAME} 8080 {$FEDORA_ADMIN_USER} ${FEDORA_ADMIN_PASS} $COLLECTION info:fedora/fedora-system:FOXML-1.1 archive $EXPORT_DIR/$COLLECTION http 2>&1 | tee $LOG_FILE
+		${FEDORA_HOME}/client/bin/fedora-export.sh ${FULL_SERVER_NAME} 8080 {$FEDORA_ADMIN_USER} ${FEDORA_ADMIN_PASS} $COLLECTION info:fedora/fedora-system:FOXML-1.1 archive $EXPORT_DIR/$COLLECTION http 2>&1 | tee -a $LOG_FILE
 	done
-	echo "" 2>&1 | tee $LOG_FILE
-	echo "Done exporting $COLLECTION" 2>&1 | tee $LOG_FILE
-	echo "" 2>&1 | tee $LOG_FILE
+	echo "" 2>&1 | tee -a $LOG_FILE
+	echo "Done exporting $COLLECTION" 2>&1 | tee -a $LOG_FILE
+	echo "" 2>&1 | tee -a $LOG_FILE
 done
-echo "" 2>&1 | tee $LOG_FILE
-echo "Finished fedora collection export: $COLLECTION on `date`" 2>&1 | tee $LOG_FILE
+echo "" 2>&1 | tee -a $LOG_FILE
+echo "Finished fedora collection export: $COLLECTION on `date`" 2>&1 | tee -a $LOG_FILE
